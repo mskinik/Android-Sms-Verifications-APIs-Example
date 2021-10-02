11e5d3babdfedb637312fad9c4d1ea9a6581b3a9
Mustafa Süleyman Kınık <suleymankinik@outlook.com>
2021-10-02 18:52:56 +0300

    Deleted logs

--- app/src/main/java/com/mustafasuleymankinik/smsreceiver/SmsBroadCastReceiver.kt
+++ app/src/main/java/com/mustafasuleymankinik/smsreceiver/SmsBroadCastReceiver.kt
@@ -15,14 +15,10 @@ class SmsBroadCastReceiver: BroadcastReceiver() {
     lateinit var smsListener1: SmsListener
 
     fun setListener(smsListener: SmsListener){
-//        Log.d("TAG", "setListener: girdi6 check${this.smsListener != null} ")
-        Log.d("TAG", "setListener: girdi6 ${smsListener}")
         this.smsListener1 = smsListener
-            Log.d("TAG", "setListener: girdi6 check${this.smsListener1 != null} ")
     }
 
     override fun onReceive(p0: Context?, p1: Intent?) {
-        Log.d("TAG", "onReceive: girdi6")
         if(SmsRetriever.SMS_RETRIEVED_ACTION == p1?.action)
         {
             val bundle = p1.extras
@@ -32,12 +28,10 @@ class SmsBroadCastReceiver: BroadcastReceiver() {
 
                 CommonStatusCodes.SUCCESS -> {
                     val message = bundle.get(SmsRetriever.EXTRA_SMS_MESSAGE)
-                    Log.d("TAG", "onReceive: girdi6 succes ${message}")
                     smsListener1.smsCode(message as String)
-                    //smsListener.smsCode("message as String")
                 }
-                CommonStatusCodes.TIMEOUT -> print("")
-                else -> print("")
+                CommonStatusCodes.TIMEOUT -> print("Time Out")
+                else -> print("Other unsuccessful issue")
             }
         }
     }
